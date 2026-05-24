8c5f7b5d631e4b3c663f966298921ad7ab453ee4
vivdiwakar <vivdiwakar@users.noreply.github.com>
2026-05-24 13:17:26 -0600

    Semantics + linting

--- src/oxide_stats.rs
+++ src/oxide_stats.rs
@@ -2,13 +2,13 @@ use statrs::statistics::Statistics;
 use rand::Rng;
 use rand_distr::{Distribution, StandardNormal};
 
-pub fn get_daily_return_stats(rets_list: &Vec<f64>) -> (f64, f64, f64, f64, f64, f64) {
+pub fn get_daily_return_stats(rets_list: &[f64]) -> (f64, f64, f64, f64, f64, f64) {
     let mean: f64 = Statistics::mean(rets_list);
     let min: f64 = Statistics::min(rets_list);
     let max: f64 = Statistics::max(rets_list);
     let stdev_p: f64 = Statistics::population_std_dev(rets_list);
     let var_p: f64 = Statistics::population_variance(rets_list);
-    let drift: f64 = mean - (&var_p / 2.0);
+    let drift: f64 = mean - (var_p / 2.0);
 
     (mean, min, max, var_p, stdev_p, drift)
 }
